b581bb3aeb0e3bdc5b6b8f0e095b888bca66315f
Patrick Longo <ck.longo@bol.com.br>
2019-11-20 13:57:35 -0300

    Att.

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,10 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import api from '~/services/api';
 
-export default all([]);
+export function* signIn({ payload }) {
+    const { email, password } = payload;
+
+    const response = yield call(api.post);
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
