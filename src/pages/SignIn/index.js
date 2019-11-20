6c6d0318c356226d81d3c261a964de9d80444cb9
Patrick Longo <ck.longo@bol.com.br>
2019-11-20 11:51:09 -0300

    Redux and Saga config

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-    return <h1>Login</h1>;
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
+    const schema = Yup.object().shape({
+        email: Yup.string()
+            .email('Insira um e-mail valido')
+            .required('O e-mail é obrigatório'),
+        password: Yup.string().required('A senha é obrigatória'),
+    });
+
+    return (
+        <>
+            <img src={logo} alt="GoBarber" />
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="email" type="email" placeholder="* Seu e-mail" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="* Sua senha secreta"
+                />
+                <button type="submit">Acessar</button>
+                <Link to="/register">Criar conta gratuita</Link>
+            </Form>
+        </>
+    );
 }
