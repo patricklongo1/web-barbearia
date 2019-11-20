a9ba12540e068fac3a4c40d77ae1fde0b0a73de0
Patrick Longo <ck.longo@bol.com.br>
2019-11-20 16:53:29 -0300

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Content, Profile } from './styles';
+import logo from '~/assets/logo2.svg';
+
+export default function Header() {
+    return (
+        <Container>
+            <Content>
+                <nav>
+                    <img src={logo} alt="gobarber" />
+                    <Link to="/dashboard">DASHBOARD</Link>
+                </nav>
+
+                <aside>
+                    <Profile>
+                        <div>
+                            <strong>Patrick Longo</strong>
+                            <Link to="/profile">Meu perfil</Link>
+                        </div>
+                        <img
+                            src="https://api.adorable.io/avatars/50/abott@adorable.png"
+                            alt="Patrick Longo"
+                        />
+                    </Profile>
+                </aside>
+            </Content>
+        </Container>
+    );
+}
